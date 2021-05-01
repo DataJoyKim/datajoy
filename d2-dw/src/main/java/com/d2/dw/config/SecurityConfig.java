dec9234925a5308c6c8c3ef0eb54261637ae9805
김낙영 <@DESKTOP-CEIOAN1>
2021-05-02 03:56:08 +0900

    2021-05-02 1) Spring Security 추가

---
+++ d2-dw/src/main/java/com/d2/dw/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.d2.dw.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.mvcMatchers("/").permitAll()
+			.mvcMatchers("/admin").hasRole("ADMIN")
+			.anyRequest().authenticated();
+	}
+}

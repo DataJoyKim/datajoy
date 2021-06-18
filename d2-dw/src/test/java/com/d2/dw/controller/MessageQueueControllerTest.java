a3a319bcb079e2250b430d30aafc471d08d4821c
김낙영 <ks25ny@gmail.com>
2021-06-18 23:38:55 +0900

    메시지 큐 테스트 클래스생성

---
+++ d2-dw/src/test/java/com/d2/dw/controller/MessageQueueControllerTest.java
@@ -0,0 +1,14 @@
+package com.d2.dw.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MessageQueueControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

dd7f6a73f17b387845d12b50c5f97af22445134d
김낙영 <ks25ny@gmail.com>
2021-06-23 23:24:03 +0900

    메시지 큐 필터 추가

--- d2-dw/src/main/java/com/d2/dw/filter/MessageQueueFilter.java
+++ d2-dw/src/main/java/com/d2/dw/filter/MessageQueueFilter.java
@@ -1,5 +1,26 @@
 package com.d2.dw.filter;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import com.d2.dw.config.MessageQueuePolicy;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class MessageQueueFilter {
 
+	private final RabbitTemplate rabbitTemplate;
+	
+	private final MessageQueuePolicy messageQueuePolicy;
+	
+	public void getMessageQueueTest() {
+		for(int i=0; i<10000; i++) {
+			String message = "message queue test_"+i;
+			System.out.println(message);
+			rabbitTemplate.convertAndSend(messageQueuePolicy.getExchangeName(), messageQueuePolicy.getRoutingKey(), message);
+		}
+		System.out.println("mq call after...");
+	}
 }

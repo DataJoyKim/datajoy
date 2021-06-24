39ed52d5e20c1b3aa00652b020d50902f5cc74f7
김낙영 <ks25ny@gmail.com>
2021-06-25 01:48:10 +0900

    Filter 구현

--- d2-dw/src/main/java/com/d2/dw/filter/MessageQueueFilter.java
+++ d2-dw/src/main/java/com/d2/dw/filter/MessageQueueFilter.java
@@ -22,20 +22,22 @@ public class MessageQueueFilter implements Filter {
 	private final RabbitTemplate rabbitTemplate;
 	
 	private final MessageQueuePolicy messageQueuePolicy;
-	
-	public void getMessageQueueTest() {
-		for(int i=0; i<10000; i++) {
-			String message = "message queue test_"+i;
-			System.out.println(message);
-			rabbitTemplate.convertAndSend(messageQueuePolicy.getExchangeName(), messageQueuePolicy.getRoutingKey(), message);
-		}
-		System.out.println("mq call after...");
-	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
 		
+		// HttpReuqest Wrapper
+		
+		// message queue send
+		String message = "message queue test";
+		try {
+			rabbitTemplate.convertAndSend(messageQueuePolicy.getExchangeName(), messageQueuePolicy.getRoutingKey(), message);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		chain.doFilter(request, response);
 	}
 }

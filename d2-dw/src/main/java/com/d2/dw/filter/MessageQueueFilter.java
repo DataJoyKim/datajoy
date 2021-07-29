5692f9e79679fc44f14dde35e9966c0d36c9d9b7
김낙영 <ks25ny@gmail.com>
2021-07-29 10:45:01 +0900

    ServletRequestUtil 추가

--- d2-dw/src/main/java/com/d2/dw/filter/MessageQueueFilter.java
+++ d2-dw/src/main/java/com/d2/dw/filter/MessageQueueFilter.java
@@ -1,9 +1,6 @@
 package com.d2.dw.filter;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -16,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import com.d2.dw.rabbitmq.MessageQueuePolicy;
 import com.d2.dw.rabbitmq.SendMessage;
+import com.d2.dw.util.ServletRequestUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -39,7 +37,7 @@ public class MessageQueueFilter implements Filter {
 		message.setPort(request.getServerPort());
 		message.setRemoteIp(request.getRemoteAddr());
 		message.setRemoteHost(request.getRemoteHost());
-		message.setRequestBody(getRequestBody(request));
+		message.setRequestBody(ServletRequestUtil.getRequestBody(request));
 		message.setUserId("ks13ny");
 
 		// message queue send
@@ -48,36 +46,4 @@ public class MessageQueueFilter implements Filter {
 		// do filter
 		chain.doFilter(request, response);
 	}
-	
-	private String getRequestBody(ServletRequest request) throws IOException {
-		 
-        String body = null;
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = null;
- 
-        try {
-            InputStream inputStream = request.getInputStream();
-            if (inputStream != null) {
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                char[] charBuffer = new char[128];
-                int bytesRead = -1;
-                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-                    stringBuilder.append(charBuffer, 0, bytesRead);
-                }
-            }
-        } catch (IOException ex) {
-            throw ex;
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException ex) {
-                    throw ex;
-                }
-            }
-        }
- 
-        body = stringBuilder.toString();
-        return body;
-    }
 }

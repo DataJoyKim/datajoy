d307d013036a99d0bdf249c9e8d7613f3a5872bd
김낙영 <ks25ny@gmail.com>
2021-08-05 23:57:39 +0900

    BoardTreeDTO Response 변경

--- d2-dw/src/main/java/com/d2/dw/dto/BoardTreeDTO.java
+++ d2-dw/src/main/java/com/d2/dw/dto/BoardTreeDTO.java
@@ -10,36 +10,41 @@ import com.d2.dw.code.BoardStatus;
 import com.d2.dw.domain.Board;
 import com.d2.dw.domain.EntityCreateUpdateData;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 public class BoardTreeDTO {
 	
-	private Long id;
-	
-	private BoardStatus status;
-	
-	private String title;
-	
-	private String content;
-	
-	private List<BoardTreeDTO> childList = new ArrayList<>();	
-
-	private EntityCreateUpdateData entityCreateUpdateData;
-	
-	public static BoardTreeDTO convert(Board board) {
-		if(board == null) return null;
+	@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+	public class Response {
+		private Long id;
 		
-		ModelMapper mapper = new ModelMapper();
-		mapper.createTypeMap(Board.class, BoardTreeDTO.class)
-				.addMappings(mapping -> mapping.skip(BoardTreeDTO::setChildList));
+		private BoardStatus status;
 		
-		BoardTreeDTO boardTreeDTO = mapper.map(board, BoardTreeDTO.class);
-		boardTreeDTO.setChildList(board.getChildList().stream()
-											.map(o -> BoardTreeDTO.convert(o))
-											.collect(Collectors.toList()));
+		private String title;
 		
-		return boardTreeDTO;
+		private String content;
+		
+		private List<BoardTreeDTO> childList = new ArrayList<>();	
+	
+		private EntityCreateUpdateData entityCreateUpdateData;
+		
+		public static BoardTreeDTO.Response convert(Board board) {
+			if(board == null) return null;
+			
+			ModelMapper mapper = new ModelMapper();
+			mapper.createTypeMap(Board.class, BoardTreeDTO.Response.class)
+					.addMappings(mapping -> mapping.skip(BoardTreeDTO.Response::setChildList));
+			
+			BoardTreeDTO.Response boardTreeDTO = mapper.map(board, BoardTreeDTO.Response.class);
+			boardTreeDTO.setChildList(board.getChildList().stream()
+												.map(o -> BoardTreeDTO.Response.convert(o))
+												.collect(Collectors.toList()));
+			
+			return boardTreeDTO;
+		}
 	}
 }

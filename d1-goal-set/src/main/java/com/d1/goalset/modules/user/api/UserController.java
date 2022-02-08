cbbdef6aed63cea60a1080df936b2a2c1db67454
DataJoyKim <ks25ny@gmail.com>
2022-02-09 02:22:39 +0900

    사용자 API 변경

--- d1-goal-set/src/main/java/com/d1/goalset/modules/user/api/UserController.java
+++ d1-goal-set/src/main/java/com/d1/goalset/modules/user/api/UserController.java
@@ -1,10 +1,14 @@
-package com.d1.goalset.modules.goal.api;
+package com.d1.goalset.modules.user.api;
 
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.d1.goalset.modules.user.service.UserQueryService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,9 +17,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 
-	@GetMapping("/owner")
-	public ResponseEntity<?> getOwner() {
-		return null;
+	private final UserQueryService userQueryService;
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<?> getUser(@PathVariable Long userId) {
+		UserDto user = userQueryService.findUserBy(userId);
+		
+		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 	
 	@GetMapping("/members")

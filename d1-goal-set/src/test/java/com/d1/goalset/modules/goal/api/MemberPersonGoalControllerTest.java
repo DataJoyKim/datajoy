4070cd652db2271e869e0eb55d6e7f72ee0f2eca
DataJoyKim <ks25ny@gmail.com>
2022-03-20 23:22:42 +0900

    테스트 케이스 추가

--- d1-goal-set/src/test/java/com/d1/goalset/modules/goal/api/MemberPersonGoalControllerTest.java
+++ d1-goal-set/src/test/java/com/d1/goalset/modules/goal/api/MemberPersonGoalControllerTest.java
@@ -1,12 +1,24 @@
 package com.d1.goalset.modules.goal.api;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.MediaTypes;
+
+import com.d1.goalset.modules.goal.code.EvalWay;
+import com.d1.goalset.modules.goal.dto.GoalDto.GoalPlanWritingRequest;
+import com.d1.goalset.modules.goal.dto.GoalDto.GoalWritingRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 class MemberPersonGoalControllerTest extends BaseTest {
 
@@ -21,5 +33,48 @@ class MemberPersonGoalControllerTest extends BaseTest {
 				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("content").exists());
+	}
+	
+	@DisplayName("개인목표 승인 생성")
+	@Test
+	void personGoalApprovalApiTest() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		
+		GoalPlanWritingRequest goalPlan_1 = GoalPlanWritingRequest.builder()
+															.staYmd(LocalDate.now())
+															.endYmd(LocalDate.now())
+															.plan("계획합니다.1")
+															.build();
+		
+		GoalPlanWritingRequest goalPlan_2 = GoalPlanWritingRequest.builder()
+															.staYmd(LocalDate.now())
+															.endYmd(LocalDate.now())
+															.plan("계획합니다.2")
+															.build();
+		
+		List<GoalPlanWritingRequest> goalPlans = new ArrayList<>();
+		goalPlans.add(goalPlan_1);
+		goalPlans.add(goalPlan_2);
+		
+		GoalWritingRequest body = GoalWritingRequest.builder()
+										.goalName("목표1")
+										.contents("목표 테스트 내용")
+										.evalWayCd(EvalWay.QUALITY_EVAL)
+										.quantStdMax("최대")
+										.quantStdGoal("목표")
+										.quantStdMin("최소")
+										.weight(100)
+										.goalPlans(goalPlans)
+										.build();
+		
+		this.mockMvc.perform(post("/api/v1/person-goals")
+				.param("seasonCd", "202201")
+				.param("companyCd", "01")
+				.param("userId", "2")
+				.content(mapper.registerModule(new JavaTimeModule()).writeValueAsString(body))
+				.contentType(MediaTypes.HAL_JSON_VALUE)
+				)
+				.andDo(print())
+				.andExpect(status().isCreated());
 	}
 }

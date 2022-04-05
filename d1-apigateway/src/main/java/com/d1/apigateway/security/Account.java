c1d47c172edb125fae10a45ca7ffbf6bd5602880
DataJoyKim <ks25ny@gmail.com>
2022-04-05 23:11:12 +0900

    api gateway security 추가

---
+++ d1-apigateway/src/main/java/com/d1/apigateway/security/Account.java
@@ -0,0 +1,51 @@
+package com.d1.apigateway.security;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "user")
+public class Account {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", columnDefinition = "INTEGER")
+	private Long id;
+	
+	@Column(name = "group_cd")
+	private String groupCd;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "user_nm")
+	private String userNm;
+
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "role")
+	private String role;
+	
+	@Column(name = "sta_date")
+	private LocalDateTime staDate;
+	
+	@Column(name = "end_date")
+	private LocalDateTime endDate;
+	
+	public void encodePassword(PasswordEncoder passwordEncoder) {
+		this.password = passwordEncoder.encode(this.password);
+	}
+}
---
+++ d1-apigateway/src/main/java/com/d1/apigateway/security/JwtTokenProvider.java
@@ -0,0 +1,117 @@
+package com.d1.apigateway.security;
+
+import java.util.Base64;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+	
+	private String secretKey = "D1-AUTH";
+	
+	/**
+	 * 객체 초기화
+	 * secretKey를 Base64로 인코딩
+	 */
+	@PostConstruct
+	protected void init() {
+	    secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+	}
+	
+	/**
+	 * 인증정보 가져오기
+	 * @param token
+	 * @return 인증정보
+	 */
+	public Authentication getAuthentication(String token) {
+        Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+		String username = getSubject(token);
+		
+		Account account = new Account();
+		account.setEmail(username);
+		account.setPassword("rlaskrdud1!");
+		account.setRole((String) claims.getBody().get("role"));
+		UserDetails userDetails = new UserAccount(account);
+	    
+	    return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+	}
+	
+	/**
+	 * 토큰에서 subject 값 조회
+	 * @param token - 토큰
+	 * @return subject
+	 */
+	public String getSubject(String token) {
+	    return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+	}
+	
+	/**
+	 * Request의 Header에서 Token 값을 가져옴
+	 * @param request
+	 * @return token
+	 */
+	public String resolveToken(HttpServletRequest request) {
+	    return request.getHeader("Authorization");
+	}
+	
+	/**
+	 * response header에 Token 셋팅
+	 * @param response
+	 * @param token
+	 */
+	public void setTokenIn(HttpServletResponse response, String token) {
+        response.setHeader("Authorization", token);
+	}
+	
+	/**
+	 * 토큰 유효성 검사
+	 * 만료일 체크
+	 * @param token - jwt token
+	 * @return 유효하면 true
+	 * @exception
+	 * <pre>1) ExpiredJwtException : JWT를 생성할 때 지정한 유효기간 초과할 때.</pre>
+	 * <pre>2) UnsupportedJwtException : 예상하는 형식과 일치하지 않는 특정 형식이나 구성의 JWT일 때</pre>
+	 * <pre>3) MalformedJwtException : JWT가 올바르게 구성되지 않았을 때</pre>
+	 * <pre>4) SignatureException :  JWT의 기존 서명을 확인하지 못했을 때</pre>
+	 * <pre>5) IllegalArgumentException : </pre>
+	 */
+	public boolean validateToken(String token) {
+	    try {
+	        Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+	        return !claims.getBody().getExpiration().before(new Date());
+	    } 
+	    catch (SignatureException e) { 
+	    	return false;
+	    } 
+	    catch (MalformedJwtException e) {  
+	    	return false;
+	    } 
+	    catch (ExpiredJwtException e) {  
+	    	return false;
+	    } 
+	    catch (UnsupportedJwtException e) {  
+	    	return false;
+	    } 
+	    catch (IllegalArgumentException e) {  
+	    	return false;
+	    }
+	}
+}
